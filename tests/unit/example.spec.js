a91b712d16060e8d6beab165b15d45ec079608de
Elsar Chabaev <s0561238@htw-berlin.de>
2023-07-11 03:13:08 +0200

    HelloWorld Test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import FormularData from '@/components/FormularData.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('FormularData.vue', () => {
+describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(FormularData, {
+    const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
